724537b84c0144e74a64704070b5744114ff9053
Angela Jin <angelacqjin@gmail.com>
2019-07-01 09:53:26 -0400

    depth_of_field done

--- 04b-rasterization/post-processing.c
+++ 04b-rasterization/post-processing.c
@@ -73,8 +73,14 @@ void depth_buffer_to_image (IMAGE *img)
         for(int i = 0; i < img->width; i++)
         {
             int z;
-            z = (int) (depth_buffer[j][i] * 255.);
+//            printf("%f\n", depth_buffer[j][i]);
+
+            z = (int) (depth_buffer[j][i] * 255.0);
             img->data[j][i][R] = CLAMP(z, 0, 255);
+            img->data[j][i][G] = CLAMP(z, 0, 255);
+            img->data[j][i][B] = CLAMP(z, 0, 255);
+
+
         }
     }
 }
@@ -83,6 +89,8 @@ void depth_buffer_to_image (IMAGE *img)
     assume that size of IMAGE in == size of IMAGE out */
 void blur (IMAGE *in, IMAGE *out, float kernel[5][5])
 {
+    out->height = WIN_H;
+    out->width = WIN_W;
     for(int j = 2; j < out->height - 2; j++)
     {
         for(int i = 2; i < out->width - 2; i++)
@@ -92,6 +100,7 @@ void blur (IMAGE *in, IMAGE *out, float kernel[5][5])
             int ky = 0;
             for(int y = j - 2; y <= j + 2; y++, ky++)
             {
+                kx = 0;
                 for(int x = i - 2; x <= i + 2; x++, kx++)
                 {
                     if(y >= 0 && y < WIN_H && x >= 0 && x < WIN_W)
@@ -104,9 +113,9 @@ void blur (IMAGE *in, IMAGE *out, float kernel[5][5])
             }
 //            if(j >= 2 && i >= 2 && i < WIN_W - 2 && j < WIN_H - 2)
 //            {
-                out->data[j][i][R] = accum[R];
-                out->data[j][i][G] = accum[G];
-                out->data[j][i][B] = accum[B];
+                out->data[j][i][R] = CLAMP(accum[R], 0, 255);
+                out->data[j][i][G] = CLAMP(accum[G], 0, 255);
+                out->data[j][i][B] = CLAMP(accum[B], 0, 255);
 //            }
         }
     }
@@ -115,23 +124,19 @@ void blur (IMAGE *in, IMAGE *out, float kernel[5][5])
 
 void blend_with_mask (IMAGE *blur, IMAGE *sharp, IMAGE *mask, IMAGE *out)
 {
+    out->height = WIN_H;
+    out->width = WIN_W;
     for(int j = 0; j < out->height; j++)
     {
         for(int i = 0; i < out->width; i++)
         {
-            float z = mask->data[j][i][R]; //z value [0, 1] is our blend weight
-//            out->data[j][i][R] = CLAMP(z * blur->data[j][i][R] +
-//                                (1 - z) * sharp->data[j][i][R], 0, 255);
-//            out->data[j][i][G] = CLAMP(z * blur->data[j][i][G] +
-//                                (1 - z) * sharp->data[j][i][G], 0, 255);
-//            out->data[j][i][B] = CLAMP(z * blur->data[j][i][B] +
-//                                (1 - z) * sharp->data[j][i][B], 0, 255);
-                        out->data[j][i][R] = z * blur->data[j][i][R] +
-                                            (1 - z) * sharp->data[j][i][R];
-                        out->data[j][i][G] = z * blur->data[j][i][G] +
-                                            (1 - z) * sharp->data[j][i][G];
-                        out->data[j][i][B] = z * blur->data[j][i][B] +
-                                            (1 - z) * sharp->data[j][i][B];
+            float z = mask->data[j][i][R] / 255.0; //z value [0, 1] is our blend weight
+            out->data[j][i][R] = CLAMP( z * blur->data[j][i][R] +
+                                       (1 - z) * sharp->data[j][i][R], 0, 255);
+            out->data[j][i][G] = CLAMP(z * blur->data[j][i][G] +
+                                       (1 - z) * sharp->data[j][i][G], 0, 255);
+            out->data[j][i][B] = CLAMP(z * blur->data[j][i][B] +
+                                (1 - z) * sharp->data[j][i][B], 0, 255);
 
         }
     }
@@ -143,24 +148,21 @@ void depth_of_field (void)
     texture03.height = WIN_H;
 
     int num_blurs = 1;
-    color_buffer_to_image(&texture00);
+    color_buffer_to_image(&texture00); //sharp
     
-    color_buffer_to_image(&texture01);
+    color_buffer_to_image(&texture01); //pingpong 1
     depth_buffer_to_image(&texture02);
-    for(int i = 0; i < 1; i++)
+
+    for(int i = 0; i < num_blurs; i++)
     {
-        printf("ah\n");
         blur(&texture01, &texture03, gaussian_kernel);
         blur(&texture03, &texture01, gaussian_kernel);
+
     }
-//    printf("blurring done\n");
-//
-    blend_with_mask (&texture01, &texture00, &texture02, &texture03);
-//    printf("blending done\n");
+    //blend sharp and blurry images using depth buffer values
+    blend_with_mask(&texture01, &texture00, &texture02, &texture03);
 
     image_to_color_buffer (&texture03);
-    printf("done\n");
-
 }
 
 
