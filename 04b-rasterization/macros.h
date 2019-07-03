6997fb13e091007f914cfe4b0027bfe86ac50394
Angela Jin <angelacqjin@gmail.com>
2019-07-03 18:31:32 -0400

    clean up macros.h

--- 04b-rasterization/macros.h
+++ 04b-rasterization/macros.h
@@ -1,9 +1,11 @@
 #ifndef MACROS_H
 #define MACROS_H
 
+/* screen */
 #define WIN_W 800
 #define WIN_H 800
 
+/* coords */
 #define R 0
 #define G 1
 #define B 2
@@ -17,11 +19,12 @@
 #define S 0
 #define T 1
 
+/* general */
+#define PI 3.1415926
+#define EPSILON 0.0001
 #define FALSE 0
 #define TRUE 1
 
-#define PI 3.1415926
-#define EPSILON 0.0001
 #define ABS(_a) (((_a) < 0.0) ? -(_a) : (_a))
 #define MAX(_a,_l) (_a < _l ? _l : _a)
 #define CLAMP(a,l,h) ((a) < (l) ? (l) : ((a) > (h) ? (h) : (a)))
