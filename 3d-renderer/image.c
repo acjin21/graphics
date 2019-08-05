e35b70cbe1a6e41da5cc56e8b40da9354d34bfed
Angela Jin <angelacqjin@gmail.com>
2019-08-05 15:19:20 -0400

    image.c

--- 3d-renderer/image.c
+++ 3d-renderer/image.c
@@ -489,8 +489,6 @@ void oil_transfer (IMAGE *input, IMAGE *output)
 #define TILE_SIZE 50
 void tiling (IMAGE *input, IMAGE *output)
 {
-//    clear_texture(output, 1, 1, 1, 1);
-
     int x, y, dx, dy;
     int ox, oy, nx, ny;
     
